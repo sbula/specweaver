b2cb1e334002ab379e6fa667a28118dc078fe2f5
Steve Bula <steve.bula@gmail.com>
2026-03-29 21:33:24 +0200

    rm temp files

--- run_unit_tests.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-with open("pytest_unit_full.txt", "w", encoding="utf-8") as f:
-    result = subprocess.run(["pytest", "tests/unit", "-v", "--tb=short"], stdout=f, stderr=subprocess.STDOUT, text=True)
-    sys.exit(result.returncode)
